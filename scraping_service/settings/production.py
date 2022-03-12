fe32584d15ad543e8b874986b9982d58fd0a38fc
Timofey <tima.rus-2007@mail.ru>
2022-03-12 14:44:02 +0500

    Heroku (add error page) #12

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -142,7 +142,4 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, '/static/'),
-# )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
