76ace1b5362c0f2899cc4ba96dfb3e34bb0adaf3
Timofey <tima.rus-2007@mail.ru>
2022-03-12 14:17:39 +0500

    Heroku #10

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -141,8 +141,8 @@ LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
